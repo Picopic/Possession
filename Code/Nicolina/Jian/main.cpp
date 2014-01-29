fe046c716e9c149923ad3ac396c5512b5f13ad93
Picopic <pico@live.se>
2014-01-29 23:19:44 +0100

    Läskig, men animerad, sprite.
    Har dock ingen timer som bestämmer hur ofta han skall byta frame, så han
    är lite party.

--- Code/Nicolina/Jian/main.cpp
+++ Code/Nicolina/Jian/main.cpp
@@ -1,40 +1,66 @@
-//main.cpp
 
-#include <SFML/Graphics.hpp>
+
+#include "stdafx.h"
+#include <string>
 #include <iostream>
-#include <vector>
 
-int main()
+int _tmain(int argc, _TCHAR* argv[])
 {
-	sf::ContextSettings settings;
-	settings.antialiasingLevel = 8;
+	enum Direction { Down, Left, Right, Up};
 
-	sf::RenderWindow window(sf::VideoMode(1280, 720), "Jian", sf::Style::Default, settings);
+	sf::Vector2i source(1, Down);
+
+	sf::RenderWindow window(sf::VideoMode(1024, 640), "GAME");
+
+	window.setKeyRepeatEnabled(false);
+	
+	sf::Sprite playerSprite;
+	sf::Texture pTexture;
+	if(!pTexture.loadFromFile("super_cool_sprite.png"))
+		std::cout << "Error could not load image" << std::endl;
+
+	playerSprite.setTexture(pTexture);
+	playerSprite.setPosition(100, 100);
 
 	while(window.isOpen())
 	{
+		//Event handling
 		sf::Event event;
-
 		while(window.pollEvent(event))
 		{
-			switch(event.type)
-			{
+			switch(event.type) {
 			case sf::Event::Closed:
 				window.close();
 				break;
-
-			case sf::Event::KeyPressed:
-				//pressing escape should shut down the program
-				if(event.key.code == sf::Keyboard::Escape)
-				{
-					window.close();
-				}
+			case sf::Keyboard::Escape:
+				window.close();
 				break;
+			case sf::Event::KeyPressed:
+				if(event.key.code == sf::Keyboard::Up)
+					source.y = Up;
+				else if(event.key.code == sf::Keyboard::Down)
+					source.y = Down;
+				else if(event.key.code == sf::Keyboard::Right)
+					source.y = Right;
+				else if (event.key.code == sf::Keyboard::Left)
+					source.y = Left;
+					break;
+				
 			}
 		}
+		source.x++;
+		if(source.x * 32 >= pTexture.getSize().x)
+			source.x = 0;
 
+
+		playerSprite.setTextureRect(sf::IntRect(source.x * 32, source.y * 32, 32, 32));
+		window.clear(sf::Color(0x11, 0x22, 0x33, 0xff));
+		window.draw(playerSprite);
+		window.display();
 		window.clear();
 
-		window.display();
 	}
+
+	return 0;
 }
+
